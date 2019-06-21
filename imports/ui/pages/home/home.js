7168f9ad817ebc33e0079b155c2e27e30eab426f
Luigy08 <luigy9696@gmail.com>
2019-06-20 20:59:27 -0600

    tst

--- imports/ui/pages/home/home.js
+++ imports/ui/pages/home/home.js
@@ -1,4 +1,17 @@
 import './home.html';
 import '../../components/hello/hello';
 import '../../components/info/info';
-import '../../components/flight/flight';
+import { Template } from 'meteor/templating';
+import { Package } from '../../../api/package/package';
+
+Template.App_home.onCreated(() => {
+  Meteor.subscribe('package.all');
+});
+Template.App_home.helpers({
+  paquete () {
+    return Package.find({ origin: 'hola', destination: 'hola' });
+  },
+  hola () {
+    return 'hola como estas ......';
+  }
+});

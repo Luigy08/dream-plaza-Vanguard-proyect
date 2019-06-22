275975863ffb41cc1b62bdf12b0642220bd92f32
Walther Carrasco <waemcame@gmail.com>
2019-06-22 16:25:48 -0600

    attempt to get aerolinea

--- imports/ui/pages/home/home.js
+++ imports/ui/pages/home/home.js
@@ -7,24 +7,28 @@ import { Package } from '../../../api/package/package';
 
 Template.App_home.onCreated(() => {
   Meteor.subscribe('package.all');
+  Session.set('flights', 0);
 });
 Template.App_home.helpers({
-  paquete() {
-    const paquetes = Package.find().map(package => {
-      const { destination, final, flight, origin, start } = package;
-      return {
-        destination,
-        final,
-        airline: flight[0].airline,
-        origin,
-        start,
-        flights: flight.length()
-      };
-    });
-    console.log(paquetes);
+  paquete () {
+    // const paquetes = Package.find().map(package => {
+    //   const { destination, final, flight, origin, start } = package;
+    //   return {
+    //     destination,
+    //     final,
+    //     airline: flight[0].airline,
+    //     origin,
+    //     start,
+    //     flights: flight.length()
+    //   };
+    // });
+    // console.log(paquetes);
     return Package.find();
   },
-  hola() {
-    return 'hola como estas ......';
+  aumentarFlight () {
+    Session.set('flights', 0);
+  },
+  getFlight () {
+    Session.get('flights');
   }
 });
